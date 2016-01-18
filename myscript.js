5db5bdcf03c32001d61631019ad548797f2d3584
foxpeter <foxpeter@qq.com>
2016-01-18 23:41:59 +0800

    commit

--- myscript.js
+++ myscript.js
@@ -1,13 +1,36 @@
-$(function(){
-	$('#InitCartUrl').after('<button id="JDUHBT" class="btn-append">test</button>');
-	$('#JDUHBT').click(function(){
+(function(window){
+	var url = window.location.href;
+	var re = /http.*item\.jd\.com\/(.*)\.html.*/;
+	var reVal = url.match(re);
+	if(reVal != null){
+		var skuId = reVal[1];
+
 		$.ajax({
-			type: "POST",
-			url: "http://media.jd.com/gotoadv/selectProMediaType",
-			data: "id=0&type=4&status=1",
+			type: "get",
+			url: "http://media.jd.com/gotoadv/goods?pageIndex=&pageSize=10&property=&sort=&adownerType=&pcRate=&wlRate=&category=&category1=0&condition=0&keyword=" + skuId,
 			success: function(msg){
-				alert( "Data Saved: " + JSON.stringify(msg) );
+				var $html = $(msg);
+				var rate = $html.find('.basic.text-center.background_fff_k tbody tr td:eq(2)').html() ;
+				var rates = rate.split('<div></div>');
+				var comm = $html.find('.basic.text-center.background_fff_k tbody tr td:eq(3)').html() ;
+				var comms = comm.split('<div></div>');
+
+				$('#summary-service').after(
+					'<div id="summary-service"  clstag="shangpin|keycount|product|fuwu_1" style="background-color: #EEEEEE;">' +
+						'<div class="dt">京东联盟：</div>' +
+						'<table><tbody><tr><td>'
+							+ rates[0] + ' = ' + comms[0].replace('PC： ','') +
+						'</td><td>'
+							+ rates[1] + ' = ' + comms[1].replace('无线： ','') +
+						'</td></tr></tbody></table>' +
+					'</div>');
+
+				//$('#InitCartUrl').after('<button id="JDUHBT" class="btn-append">' + rate + comm + '</button>');
 			}
 		});
-	})
-});
+	}
+
+	//$('#JDUHBT').click(function(){
+	//
+	//})
+})(window);
